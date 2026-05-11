cf3ce86e18c8f67a630dd98f90c66555a13872dd
Guilherme Aquino Lima Barroso <guiaqui23@gmail.com>
2026-05-11 16:20:40 -0300

    Refactor installer script to use BRANCH variable

--- standalone_xanmod_installer.sh
+++ standalone_xanmod_installer.sh
@@ -4,31 +4,31 @@ mkdir xanmod_packages && cd xanmod_packages
 # Download check script
 wget https://dl.xanmod.org/check_x86-64_psabi.sh
 chmod +x "$(basename "https://dl.xanmod.org/check_x86-64_psabi.sh")"
-XANMOD_VER=$(./check_x86-64_psabi.sh | tail -c 2)
+PSABI_LV=$(./check_x86-64_psabi.sh | tail -c 2)
 
 # Choose wich version
 read -p "Type 1 for Long-Term Support or 2 for Main: " choice
 if  	[[ $choice == 1 ]]; then
-	PACKAGE="lts"
+	BRANCH="lts"
 elif	[[ $choice == 2 ]]; then
-	PACKAGE="main"
+	BRANCH="main"
 else
 echo "Invalid choice"
 	exit 1
 fi
 
 # Fetch the webpage content
-CONTENT=$(curl -s "https://sourceforge.net/projects/xanmod/files/releases/$PACKAGE/")
+CONTENT=$(curl -s "https://sourceforge.net/projects/xanmod/files/releases/$BRANCH/")
 
 # Extract the folder path for the newest package
-LATEST_RELEASE=$(echo "$CONTENT" | grep -oP '(?<=href=")/projects/xanmod/files/releases/'$PACKAGE'/[^"]+/' | head -n 1)
+LATEST_RELEASE=$(echo "$CONTENT" | grep -oP '(?<=href=")/projects/xanmod/files/releases/'$BRANCH'/[^"]+/' | head -n 1)
 
 # Extract the last part of the URL and format the output
 LAST_PART=$(echo "$LATEST_RELEASE" | grep -oP '[^/]+/$')			# Extract release tag
 PREFIX=$(echo "$LAST_PART" | grep -oP '^[0-9]+\.[0-9]+\.[0-9]+')	# Extract release number
 
 # Donwloads Xanmod packages
-curl https://sourceforge.net/projects/xanmod/rss?path=/releases/$PACKAGE/$LAST_PART$PREFIX-x64v$XANMOD_VER-xanmod1 | \
+curl https://sourceforge.net/projects/xanmod/rss?path=/releases/$BRANCH/$LAST_PART$PREFIX-x64v$PSABI_LV-xanmod1 | \
 grep "<link>.*</link>" | \
 sed 's|<link>||;s|</link>||' | \
 grep "\.deb/download$" | \
