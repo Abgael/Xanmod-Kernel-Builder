cdfcf7db4fc79d0435d234ad3abb0913d7e2805f
Guilherme Aquino Lima Barroso <guiaqui23@gmail.com>
2026-06-24 17:27:59 -0300

    Refactor kernel build process in Xanmod_builder.sh
    Updated build process for kernel with clang and llvm tools. Added references for building with LLVM.

--- Xanmod_builder.sh
+++ Xanmod_builder.sh
@@ -6,8 +6,8 @@ git clone --depth 1 --single-branch --branch $RELEASE https://gitlab.com/xanmod/
 cd linux
 
 # Cleans previous builds
-make clean
 make mrproper
+make clean
 
 # Copies currently kernel config
 cp /boot/config-$(uname -r) .config
@@ -15,30 +15,35 @@ cp /boot/config-$(uname -r) .config
 # Overrides some kernel configurations
 source ../intel_kernel_settings.sh
 
-#Build modules
-#make modules
-
-# Avoids WARNING: Module.symvers is missing.
-#cp /usr/src/linux-headers-$(uname -r)/Module.symvers . || true
-
 # Points LSMOD variable to the modprobed-db database
-make LSMOD=$HOME/.config/modprobed.db localmodconfig
+make localmodconfig
 
-# Builds the kernel
-# Ref.:https://www.kernel.org/doc/html/v6.6/kbuild/llvm.html
-make LLVM=1 \
-KCFLAGS="-qopt-streaming-stores=auto, -ipo, -O3, -static, -fp-model=fast" \
-USERCFLAGS="-qopt-streaming-stores=auto, -ipo, -O3, -static, -fp-model=fast" \
--j$(nproc)
-#Flags above are alternetives to -fast flag
-#Ref.:https://doku.lrz.de/comparison-of-compiler-options-intel-vs-pgi-vs-gcc-11481685.html#ComparisonofCompilerOptions(intelvs.pgivs.gcc)-CompilerDirectivesfortheIntelcompiler
-#Ref.:https://www.intel.com/content/www/us/en/docs/dpcpp-cpp-compiler/developer-guide-reference/2025-2/fast.html
+# Building the the kernel
+
+make CC=clang \
+    LD=ld.lld \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    STRIP=llvm-strip \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    READELF=llvm-readelf \
+    HOSTCC=clang \
+    HOSTCXX=clang++ \
+    HOSTAR=llvm-ar \
+    HOSTLD=ld.lld \
+    -j$(nproc)
+
+#Ref.:https://docs.kernel.org/kbuild/llvm.html#building-with-llvm
+#Ref.:https://www.kernel.org/doc/html/latest/kbuild/llvm.html#building-with-llvm
 
 # Kernel modules install
 sudo make modules_install
 
 # Kernel install
 sudo make install
+
+#Ref.:https://wiki.gentoo.org/wiki/Kernel/Configuration#Setup
 
 # GRUB update
 sudo update-grub
